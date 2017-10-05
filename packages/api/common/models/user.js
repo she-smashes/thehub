fa325bed7713a395dc415cf5d0dfa9fdd62e4fe4
Uma Govindaraj <ugovi1@sapient.com>
2017-10-05 22:35:42 +0530

    defining sample mixins for models

--- packages/api/common/models/user.js
+++ packages/api/common/models/user.js
@@ -2,4 +2,52 @@
 
 module.exports = function(User) {
 
+	
+	User.listTasks = function(userId, cb) {
+		
+		User.find({
+			"filter": {
+				"where": {
+					"id": userId
+				},
+				"include": {
+					"relation": "tasks"
+				}
+			}
+		});
+		
+    };
+	User.remoteMethod('listTasks', {
+		
+		accepts: [
+		  {arg: 'userId', type: 'string'}
+		],
+		returns: {arg: 'tasks', type: 'array'},
+		http: {path:'/list-tasks', verb: 'get'}
+		
+	});
+
+	User.listEnrollments = function(userId, cb) {
+    	
+		User.find({
+			"filter": {
+				"where": {
+				  "id": userId
+				},
+				"include": {
+				  "relation": "enrollments"
+				}
+			}
+		});
+	
+	};
+	User.remoteMethod('listEnrollments', {
+		
+		accepts: [
+		  {arg: 'userId', type: 'number'}
+		],
+		returns: {arg: 'enrollments', type: 'array'},
+		http: {path:'/list-enrollments', verb: 'get'}
+		
+	});
 };
