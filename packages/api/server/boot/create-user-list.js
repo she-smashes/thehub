7b0e094be0a548ccc0a28105e8b78ee2e51c9df5
Uma Govindaraj <ugovi1@sapient.com>
2017-10-27 09:55:05 +0530

    correct boot script code

--- packages/api/server/boot/create-user-list.js
+++ packages/api/server/boot/create-user-list.js
@@ -4,59 +4,78 @@ const sampleData = require('./create-user-list.json');
 
 module.exports = function (app, cb) {
 
-  const promises = [];
+  const employeePromises = [];
+  const appadminPromises = [];
+  const sysadminPromises = [];
 
-  Object.keys(sampleData).forEach(modelName => {
-    const Model = app.models[modelName];
-    const modelItems = sampleData[modelName];
+	Object.keys(sampleData).forEach(modelName => {
+		const Model = app.models[modelName];
+		const modelItems = sampleData[modelName];
 
-    modelItems.forEach(modelItem => {
-      promises.push(new Promise(resolve => {
-        Model.upsertWithWhere({ 'username': modelItem.username }, modelItem).then(resolve);
-      }))
-    })
-  });
-  var role;
-	
-  app.models.Role.findOne({where: {name: 'employee'}}, function(err, adminRole) {
-    if(adminRole) {
-      console.log('adminRole' , adminRole);
-      role = adminRole;
-    } else {
-      console.log(err);
-    }
-  });
-
-  return Promise
-    .all(promises)
-    .then((res) => {
-		console.log('Users set up', res.length);
-		var i = 0;
-
-		const rolemappingpromises = [];
-		var roleId = role.id;
-		
-		res.forEach(usr => {
-			rolemappingpromises.push(new Promise(resolve => {
-				app.models.RoleMapping.findOne({where: {principalType: 'USER', principalId: usr.id, roleId: role.id}}, function(err, oldprincipal) {
-				if(!oldprincipal) {
-					role.principals.create({
-							principalType: app.models.RoleMapping.USER,
-							principalId: usr.id
-						}, function(err, principal) {
-							console.log('RoleMapping done for user id = ' , usr.id + ', role id = ' + role.id);
-							if(!principal) {
-								console.log(err);
-							}
-					});
-				} else {
-					console.log('RoleMapping exists for user id = ' , usr.id + ', role id = ' + role.id);
-				} 
-			})
-			
-			}))
-		});
-		
+		modelItems.forEach(modelItem => {
+			if(modelItem.role === 'employee') {
+				 employeePromises.push(new Promise(resolve => {
+					Model.upsertWithWhere({ 'email': modelItem.email }, modelItem).then(resolve);
+				}))
+			} else if(modelItem.role === 'appadmin') {
+				 appadminPromises.push(new Promise(resolve => {
+					Model.upsertWithWhere({ 'email': modelItem.email }, modelItem).then(resolve);
+				}))
+			} else if(modelItem.role === 'sysadmin') {
+				 sysadminPromises.push(new Promise(resolve => {
+					Model.upsertWithWhere({ 'email': modelItem.email }, modelItem).then(resolve);
+				}))
+			}
+			delete modelItem.role;
+		})
 	});
-	console.log('User Roles set up');
+  
+	handleRoleMapping(app, employeePromises, 'employee');
+	handleRoleMapping(app, appadminPromises, 'appadmin');
+	handleRoleMapping(app, sysadminPromises, 'sysadmin');
+
+	console.log('Sample users, roles and their mapping set up successfully');
 };
+
+function handleRoleMapping(app, promises, roleName) {
+	var role;
+	
+	app.models.Role.findOne({where: {name: roleName}}, function(err, adminRole) {
+		if(adminRole) {
+		  role = adminRole;
+		} else {
+		  console.log(err);
+		}
+	});
+
+	return Promise
+		    .all(promises)
+		    .then((res) => {
+				var i = 0;
+
+				const rolemappingpromises = [];
+				var roleId = role.id;
+		
+				res.forEach(usr => {
+					rolemappingpromises.push(new Promise(resolve => {
+						app.models.RoleMapping.findOne({where: {principalType: 'USER', principalId: usr.id, roleId: role.id}}, function(err, oldprincipal) {
+						if(!oldprincipal) {
+							role.principals.create({
+									principalType: app.models.RoleMapping.USER,
+									principalId: usr.id
+								}, function(err, principal) {
+									console.log('RoleMapping done for user id = ' , usr.id + ', username = ' + usr.username + ', role id = ' + role.id);
+									if(!principal) {
+										console.log(err);
+									}
+							});
+						} else {
+							console.log('RoleMapping exists for user id = ' , usr.id + ', role id = ' + role.id);
+						} 
+					})
+					
+					}))
+				});
+		
+			});
+}
