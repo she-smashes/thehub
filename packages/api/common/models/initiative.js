dbd3dee2bf87eeb0a139a187f6bf5ed9618a3964
Uma Govindaraj <ugovi1@sapient.com>
2017-10-30 16:53:42 +0530

    load sample users

--- packages/api/common/models/initiative.js
+++ packages/api/common/models/initiative.js
@@ -2,4 +2,11 @@
 
 module.exports = function(Initiative) {
 
+  Initiative.listInitiatives = function(cb) {
+    Initiative.find({}, cb);
+  };
+  Initiative.remoteMethod('listInitiatives', {
+    returns: {arg: 'initiatives', type: 'array'},
+    http: {path:'/list-initiatives', verb: 'get'}
+  });
 };
