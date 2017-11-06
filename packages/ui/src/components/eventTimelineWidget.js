d3fbb1c2c1c82cc417334325767363f7fe0c556f
Shalini Jha <jha.shalini.dev@gmail.com>
2017-11-06 20:06:22 +0530

    Added comments

--- packages/ui/src/components/eventTimelineWidget.js
+++ packages/ui/src/components/eventTimelineWidget.js
@@ -12,6 +12,13 @@ class EventTimelineWidget extends Component {
         this.props.getEventList(this.props.userInfo.id)
     }
 
+    /**
+     * @name resolvebackgroundColor
+     * @desc Resolves background color of an event based on it start date and end date
+     * @param {UnixTimeStamp} startDate
+     * @param {unicTimeStamp} endDate
+     * @return css property
+     */
     resolveBackgroundColor = (startDate, endDate) => {
         if(endDate < Date.now()) {
             return {margin:10, backgroundColor:"#e5e5e5", color:"silver"};
@@ -21,6 +28,12 @@ class EventTimelineWidget extends Component {
             return {margin:10, backgroundColor:"yellow"};
         }
     }
+
+    /**
+     * @name renderEvents
+     * @desc Iterates through the list of the events and renders the list of events
+     * @return Rendered events list {HTML}
+     */
     renderEvents = () => {
         return this.props.events.map((event, index) => {
             return <ListItem 
@@ -29,6 +42,7 @@ class EventTimelineWidget extends Component {
             style={this.resolveBackgroundColor(Number(event.startDate), Number(event.endDate))} />
         })
     }
+    
     
     render = () => {
         console.log(this.props)
