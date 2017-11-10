d5b79e74d4bb05998b7fb9654865ce6203b80d3d
Thenmozhi Subramaniam <tsubramaniam@sapient.com>
2017-11-10 15:43:27 +0530

    Added comment

--- packages/ui/src/components/header.js
+++ packages/ui/src/components/header.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import '../css/header.css'
 
+/**
+  Defined header
+*/
 const Header = (props) => {
+
   return (
     <nav className="navbar navbar-fixed-top ">
-      
+
       {/* <!-- Brand and toggle get grouped for better mobile display --> */}
       <div className="navbar-header">
         <h1 className="brand-title">Some Portal</h1>
