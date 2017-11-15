1db6b26173777de54a1535b3f0acbbcae39f1678
Uma Govindaraj <ugovi1@sapient.com>
2017-11-15 23:53:26 +0530

    approval and login api response

--- packages/api/common/models/task.js
+++ packages/api/common/models/task.js
@@ -1,4 +1,30 @@
 'use strict';
 
-module.exports = function(Task) {
+module.exports = function (Task) {
+    Task.listPendingTasks= function (ctx, cb) {
+
+        Task.find({
+            where: {
+                status: 'Pending'
+            }
+        }, function (err, taskInstanses) {
+            cb(null, taskInstanses);
+        });
+    };
+
+
+  Task.remoteMethod('listPendingTasks', {
+    accepts: [
+      { arg: 'ctx', type: 'object', http: { source: 'context' }}
+    ],
+    http: {
+      path: '/list-pending-tasks',
+      verb: 'get'
+    },
+    returns: {
+      arg: 'pendingTasks',
+      type: 'array'
+    }
+  }
+  );
 };
