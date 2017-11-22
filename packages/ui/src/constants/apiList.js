b940c6febcccd0d35a831ea0d38270bd9938b5a2
rarad1 <raradhyula@sapient.com>
2017-11-22 16:22:29 +0530

    api urls added back

--- packages/ui/src/constants/apiList.js
+++ packages/ui/src/constants/apiList.js
@@ -1,3 +1,12 @@
-export const GET_TASKS = "http://localhost:4000/api/tasks/list-pending-tasks";
-export const UPDATE_TASK = "http://localhost:4000/api/tasks";
+// export const GET_TASKS = "http://localhost:4000/api/tasks/list-pending-tasks";
+// export const UPDATE_TASK = "http://localhost:4000/api/tasks";
+// export const CREATE_NEW_EVENT = "http://localhost:4000/api/events";
+
+export const GET_EVENTS = "https://develop-shesmashes-thehub.herokuapp.com/api/events/list-events";
+export const LOGGIN_USER = "https://develop-shesmashes-thehub.herokuapp.com/api/users/login";
+export const GET_EVENTDETAILURL = "https://develop-shesmashes-thehub.herokuapp.com/api/events";
+export const GET_INITIATIVES = "https://develop-shesmashes-thehub.herokuapp.com/api/initiatives";
+export const CREATE_NEW_INITIATIVE = "https://develop-shesmashes-thehub.herokuapp.com/api/initiatives";
+export const GET_TASKS = "https://develop-shesmashes-thehub.herokuapp.com/api/tasks/list-pending-tasks";
+export const UPDATE_TASK = "https://develop-shesmashes-thehub.herokuapp.com/api/tasks";
 export const CREATE_NEW_EVENT = "http://localhost:4000/api/events";
