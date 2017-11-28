8020125810ff602acd06cd533164eeb028069066
Uma Govindaraj <ugovi1@sapient.com>
2017-11-28 15:27:10 +0530

    event register

--- packages/api/common/models/initiative.js
+++ packages/api/common/models/initiative.js
@@ -2,38 +2,29 @@
 
 module.exports = function (Initiative) {
 
-Initiative.disableRemoteMethodByName('upsert');
-Initiative.disableRemoteMethodByName('upsertWithWhere');
-Initiative.disableRemoteMethodByName('exists');
-Initiative.disableRemoteMethodByName('updateAll');
-Initiative.disableRemoteMethodByName('count');
-Initiative.disableRemoteMethodByName('createChangeStream');
-Initiative.disableRemoteMethodByName('replaceById');
-Initiative.disableRemoteMethodByName('replaceOrCreate');
-Initiative.disableRemoteMethodByName('prototype.__findByById__events_deleteById');
-Initiative.disableRemoteMethodByName('prototype.__findById__events_updateById');
-Initiative.disableRemoteMethodByName('prototype.__findById__events_count');
+  Initiative.disableRemoteMethodByName('upsert');
+  Initiative.disableRemoteMethodByName('upsertWithWhere');
+  Initiative.disableRemoteMethodByName('exists');
+  Initiative.disableRemoteMethodByName('updateAll');
+  Initiative.disableRemoteMethodByName('count');
+  Initiative.disableRemoteMethodByName('createChangeStream');
+  Initiative.disableRemoteMethodByName('replaceById');
+  Initiative.disableRemoteMethodByName('replaceOrCreate');
+  Initiative.disableRemoteMethodByName('prototype.__findByById__events_deleteById');
+  Initiative.disableRemoteMethodByName('prototype.__findById__events_updateById');
+  Initiative.disableRemoteMethodByName('prototype.__findById__events_count');
 
-
-
-  Initiative.listInitiatives = function (cb) {
-    Initiative.find({}, cb);
-  };
-  Initiative.remoteMethod('listInitiatives', {
-    returns: { arg: 'initiatives', type: 'array' },
-    http: { path: '/list-initiatives', verb: 'get' }
-  });
   Initiative.observe('before save', function (ctx, next) {
     if (ctx.instance) {
       console.log('updating initiative status');
-        ctx.instance.status = 'not approved'; 
+      ctx.instance.status = 'not approved';
     }
     next();
   });
   Initiative.observe('after save', function (ctx, next) {
     if (ctx.instance) {
       console.log('Saved %s#%s', ctx.Model.modelName, ctx.instance.id);
-      Initiative.app.models.Task.create({type: 'initiative', approvableId:ctx.instance.id, status: 'Pending'});
+      Initiative.app.models.Task.create({ type: 'initiative', approvableId: ctx.instance.id, status: 'Pending' });
     } else {
       console.log('Updated Initiative %s matching %j',
         ctx.Model.pluralModelName,
--- packages/ui/src/containers/eventDetailsContainer/action.js
+++ packages/ui/src/containers/eventDetailsContainer/action.js
@@ -1,4 +1,6 @@
 import { GET_EVENTDETAILS } from "../../constants/actions";
+import { GET_ENROLLMENTDETAILS } from "../../constants/actions";
+import { UPDATE_ENROLLMENTDETAILS } from "../../constants/actions";
 
 import Swagger from 'swagger-client';
 
@@ -13,14 +15,19 @@ export const getEventDetails = (eventId, access_token) => {
         },
       })
       .then((client) => {
+
+
+        let filterQuery = { include: "enrollments" };
+        filterQuery = JSON.stringify(filterQuery)
+
         return client
           .apis
           .event
-          .event_findById({ id: eventId });
+          .event_findById({ id: eventId, filter: filterQuery });
       });
   }
 }
-export const registerUserForEvent = (eventId, userId, access_token) => {
+export const registerUserForEvent = (eventId, userId, registerFlag,  enrollmentId, access_token) => {
 
   return function (dispatch) {
     return Swagger(process.env.REACT_APP_API_URI,
@@ -32,26 +39,89 @@ export const registerUserForEvent = (eventId, userId, access_token) => {
       })
       .then((client) => {
 
-        let postBody = {
-          "eventId": eventId,
-          "userId": userId,
-          "registeredOn": new Date(),
-          "enrollmentType": "1"
-        };
-        postBody = JSON.stringify(postBody);
 
-        return client
-          .apis
-          .event
-          .event_prototype___create__enrollments({ id: eventId, data: postBody });
+        if (registerFlag) {
+          let postBody = {
+            "eventId": eventId,
+            "userId": userId,
+            "registeredOn": new Date(),
+            "enrollmentType": "1"
+          };
+          postBody = JSON.stringify(postBody);
+          console.log(postBody);
+          return client
+            .apis
+            .event
+            .event_prototype___create__enrollments({ id: eventId, data: postBody });
+        } else {
+          return client
+            .apis
+            .event
+            .event_prototype___destroyById__enrollments({ id: eventId, fk: enrollmentId });
+        }
 
       });
   }
 }
 
-export const updateEventDetails = (eventDetailsInfo) => {
+export const getEnrollmentDetails = (eventId, userId, access_token) => {
+
+  return function (dispatch) {
+    return Swagger(process.env.REACT_APP_API_URI,
+      {
+        requestInterceptor: (req) => {
+          req.headers['Authorization'] = access_token;
+          return req;
+        },
+      })
+      .then((client) => {
+        console.log('eventId = ' + eventId);
+        let filterQuery = { include: "events" };
+        filterQuery = JSON.stringify(filterQuery)
+
+
+        return client
+          .apis
+          .event
+          .event_prototype___get__enrollments({ id: eventId, filter: filterQuery });
+
+      });
+  }
+}
+
+export const updateEventDetails = (userId, eventDetailsInfo) => {
+
+  let evDet = {};
+  eventDetailsInfo.registered = false;
+  if (eventDetailsInfo.enrollments != undefined) {
+    eventDetailsInfo.enrollments.map(enrollmentDetail => {
+      if (enrollmentDetail.userId === userId) {
+        eventDetailsInfo.registered = true;
+        eventDetailsInfo.enrollmentId = enrollmentDetail.id;
+      }
+    });
+  }
+
   return {
-    type: 'GET_EVENTDETAILS',
+    type: GET_EVENTDETAILS,
     payload: eventDetailsInfo
   };
 }
+
+export const updateEnrollmentDetails = (userId, enrollmentDetailsInfo) => {
+
+  let registered = false;
+  enrollmentDetailsInfo.map(enrollDetail => {
+    console.log('enrollDetail = ' + enrollDetail);
+
+    if (userId === enrollDetail.userId) {
+      registered = true;
+    }
+  });
+
+  return {
+    type: UPDATE_ENROLLMENTDETAILS,
+    payload: { 'registered': registered }
+  };
+}
+
