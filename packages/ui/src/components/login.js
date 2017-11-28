d7037333aec2414c29ddab277658a8e30d9fa41c
Thenmozhi Subramaniam <tsubramaniam@sapient.com>
2017-11-28 20:41:10 +0530

    Updated the code to redirect the user to dashboard when logged in user access the login page.

--- packages/ui/src/components/login.js
+++ packages/ui/src/components/login.js
@@ -32,7 +32,10 @@
        open: false,
        isLoading: false
      };
-
+     if(this.props.user != null)
+     {
+       // add the comment to redirect to dashboard page
+     }
    }
    handleOpen = () => {
      this.setState({open: true});
