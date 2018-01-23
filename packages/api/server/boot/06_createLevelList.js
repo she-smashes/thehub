637f5d98f86fd06e613a01800a4d0371b643962d
Uma Govindaraj <ugovi1@sapient.com>
2018-01-23 10:02:13 +0530

    review comments

--- packages/api/server/boot/06_createLevelList.js
+++ packages/api/server/boot/06_createLevelList.js
@@ -4,29 +4,50 @@ const sampleLevelData = require('./createLevelList.json');
 
 module.exports = function(app, cb) {
   const promises = [];
-
+  const catPromises = [];
   Object.keys(sampleLevelData).forEach(modelName => {
     const Model = app.models[modelName];
     const modelItems = sampleLevelData[modelName];
-
     modelItems.forEach(modelItem => {
-      app.models.category.findOne({where: {'name': modelItem.category}},
-        function(err, levelCategory) {
+      console.log(modelItem.category);
+      catPromises.push(new Promise(function(resolve) {
+        app.models.category.findOne(
+          {
+            where: {
+              'name': modelItem.category,
+            },
+          }, function(err, levelCategory) {
           if (levelCategory) {
             modelItem.categoryId = levelCategory.id;
             delete modelItem.category;
-            promises.push(Model.upsertWithWhere({
-              'name': modelItem.name,
-              'categoryId': modelItem.categoryId,
-            }, modelItem));
           }
+          resolve();
         });
+      }));
     });
   });
 
-  Promise.all(promises)
+  Promise.all(catPromises)
     .then((res) => {
-      console.log('Levels set up ' + res.length);
-      return cb();
+      Object.keys(sampleLevelData).forEach(modelName => {
+        const Model = app.models[modelName];
+        const modelItems = sampleLevelData[modelName];
+        modelItems.forEach(modelItem => {
+          promises.push(new Promise(function(resolve) {
+            Model.upsertWithWhere({
+              'name': modelItem.name,
+              'categoryId': modelItem.categoryId,
+            }, modelItem, function(err, i) {
+              console.log(i);
+              resolve();
+            });
+          }));
+        });
+      });
+      Promise.all(promises)
+        .then((res) => {
+          console.log('Levels set up ' + res.length);
+          cb();
+        });
     });
 };
